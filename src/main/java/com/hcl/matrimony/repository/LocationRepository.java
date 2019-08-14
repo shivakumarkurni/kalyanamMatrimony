0bf10a0e4050ecbb527084a8491ecfca23e2b553
bindubogarapu <bogarapu.bindu@gmail.com>
2019-08-14 14:09:05 +0530

    repositorys added

--- src/main/java/com/hcl/matrimony/repository/LocationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hcl.matrimony.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hcl.matrimony.entity.Location;
-
-public interface LocationRepository  extends JpaRepository<Location, Long>{
-
-}
--- src/main/java/com/hcl/matrimony/repository/OccupationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hcl.matrimony.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hcl.matrimony.entity.Occupation;
-
-public interface OccupationRepository  extends JpaRepository<Occupation, Long>{
-
-}
